a03c682ad30ecc6895ce7b84d7aa0f95ba8a6b3e
Bridget Brevik <bridgetbrevik@outlook.com>
2021-07-15 08:02:58 -0500

    updated main.js

--- client-side/js/main.js
+++ client-side/js/main.js
@@ -5,7 +5,8 @@ async function getRapName(){
     try{
         // const res = await fetch('http://localhost:8000/api/savage')
         // const res = await fetch('http://localhost:8000/api/rappers')
-        const res = await fetch(`http://localhost:8000/api/rappers/${rapName}`)
+        // const res = await fetch(`http://localhost:8000/api/rappers/${rapName}`)
+        const res = await fetch(`https://rap-api-100devs-take2.herokuapp.com/api/rappers/${rapName}`)
         const data = await res.json()
         document.querySelector('h2').innerText = data.birthName
     }catch(err){
